54c72f3521f01df753fa2a41f4a0f838bc734ffa
mirmohsen <mirmohsenmirazimitarki78@gmail.com>
2024-06-10 00:26:21 +0330

    A simple code to add to a Telegram channel and send messages that contain the tag #bug to Jira

---
+++ app.js
@@ -0,0 +1,92 @@
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+async function createJiraIssue(title, description, attachments) {
+	const issueData = {
+		fields: {
+			project: {
+				key: process.env.JIRA_PROJECT_TOKEN,
+			},
+			summary: title,
+			description: description,
+			issuetype: {
+				name: 'Bug',
+			},
+		},
+	};
+
+	const issueResponse = await axios.post(
+		`${process.env.JIRA_BASE_URL}/rest/api/2/issue`,
+		issueData,
+		{
+			auth: {
+				username: process.env.JIRA_EMAIL,
+				password: process.env.JIRA_API_TOKEN,
+			},
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+
+	const issueKey = issueResponse.data.key;
+
+	for (const attachment of attachments) {
+		await axios.post(
+			`${process.env.JIRA_BASE_URL}/rest/api/2/issue/${issueKey}/attachments`,
+			{
+				uri: attachment.url,
+			},
+			{
+				auth: {
+					username: process.env.JIRA_EMAIL,
+					password: process.env.JIRA_API_TOKEN,
+				},
+				headers: {
+					'X-Atlassian-Token': 'no-check',
+				},
+			}
+		);
+	}
+
+	return issueResponse.data;
+}
+
+bot.on('channel_post', async (ctx) => {
+	console.log('>>>>> Starting bot!');
+
+	const message = ctx.channelPost;
+	const text = message.caption;
+
+	if (text && text.includes('#bug')) {
+		const title = text.split('\n')[0];
+		const description = text;
+
+		let attachments = [];
+		if (message.photo) {
+			const fileId = message.photo[message.photo.length - 1].file_id;
+			const fileUrl = await ctx.telegram.getFileLink(fileId);
+			attachments.push({ url: fileUrl });
+		}
+
+		if (message.video) {
+			const fileId = message.video.file_id;
+			const fileUrl = await ctx.telegram.getFileLink(fileId);
+			attachments.push({ url: fileUrl });
+		}
+
+		await createJiraIssue(title, description, attachments)
+			.then((response) => {
+				ctx.reply('Task created in Jira successfully!');
+			})
+			.catch((error) => {
+				console.error(error);
+				ctx.reply('Failed to create task in Jira.');
+			});
+	}
+});
+
+bot.launch();
